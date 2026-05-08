cfaa5b8e7ca4ecab14b831712877d0048d6cb609
au2idsweetdanceshop-lab <au2idsweetdanceshop@gmail.com>
2026-05-09 02:15:49 +0700

    Refactor video list initialization in get-videos.js

--- api/get-videos.js
+++ api/get-videos.js
@@ -1,15 +1,11 @@
-// File: api/get-videos.js
-
 export default function handler(req, res) {
-    // Di sini adalah daftar video yang akan muncul di feed
-    const daftarVideo = [
-        { 
-            id: 'vid-01', 
-            caption: 'Review Joki Ballroom AU2ID - 100% Aman', 
-            file_id: 'AAMCBQADGQEDFhK7af4kTVxdieF6AT4yJltZlkyQURYAAh4cAAKO_PBXiVxpfJ40tJoBAAdtAAM7BA' // <-- Masukkan File ID dari @ShowJsonBot
-        },
-    ];
+  const daftarVideo = [
+    {
+      id: 'vid-01',
+      caption: 'Review Joki Ballroom AU2ID - 100% Aman',
+      file_id: 'AAMCBQADGQEDFhK7af4kTVxdieF6AT4yJltZlkyQURYAAh4cAAKO_PBXiVxpFJ40tJoBAAdtAAM7BA'
+    }
+  ];
 
-    // Mengirim daftar ini dalam format JSON agar bisa dibaca index.html
-    res.status(200).json(daftarVideo);
+  res.status(200).json(daftarVideo);
 }
