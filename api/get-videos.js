55cb44ddab226eb2c463b94694e917e1ffc96a59
au2idsweetdanceshop-lab <au2idsweetdanceshop@gmail.com>
2026-05-09 13:10:32 +0700

    Replace file_id for video entries
    Updated file_id for multiple videos in the videoList.

--- api/get-videos.js
+++ api/get-videos.js
@@ -4,27 +4,27 @@ export default function handler(req, res) {
       {
         id: 'vid-01',
         caption: 'bernadya😍',
-        file_id: 'BAACAgUAAxkBAAEpCwpp_jkrBzWZxIwUrvnoIX0Fe21cwgACHhwAAo788Fd8NusBjfzGzDsE'
+        file_id: 'BAACAgUAAxkBAAEpC1Np_kHktRRWhLSDk4myyaCwIOQdsAACdRwAAo788FcPiEXe7DUvoTsE'
       },
       {
         id: 'vid-02',
         caption: 'kediriku❤',
-        file_id: 'BAACAgUAAxkBAAEpCylp_j9VEmcpPxeOnFMcmKCqtn7tEgACGhwAAo788FepuhsTxIbOMDsE'
+        file_id: 'BAACAgUAAxkBAAEpC1Np_kHktRRWhLSDk4myyaCwIOQdsAACdRwAAo788FcPiEXe7DUvoTsE'
       },
       {
         id: 'vid-03',
         caption: 'sleman kota kecil kebanggan😍',
-        file_id: 'BAACAgUAAxkBAAEpC0Bp_j_qU7gDNaJfEHfIWkiSbF46TwACGRwAAo788Fe0V3hlX8jJRDsE'
+        file_id: 'BAACAgUAAxkBAAEpC1Np_kHktRRWhLSDk4myyaCwIOQdsAACdRwAAo788FcPiEXe7DUvoTsE'
       },
       {
         id: 'vid-04',
         caption: 'say you want the moon🤩',
-        file_id: 'BAACAgUAAxkBAAEpC0Zp_kBTNfQOrvNaEL6773YqmT8qHAACNhwAAo788Ff4mE5vZLBOrDsE'
+        file_id: 'BAACAgUAAxkBAAEpC1Np_kHktRRWhLSDk4myyaCwIOQdsAACdRwAAo788FcPiEXe7DUvoTsE'
       },
       {
         id: 'vid-05',
         caption: 'frekuensi 17.1',
-        file_id: 'BAACAgUAAxkBAAEpC09p_kD--Rh6JLHQX6hzmjvak0pMTwACdBwAAo788FePlD6EpuOsvTsE'
+        file_id: 'BAACAgUAAxkBAAEpC1Np_kHktRRWhLSDk4myyaCwIOQdsAACdRwAAo788FcPiEXe7DUvoTsE'
       },
       {
         id: 'vid-06',
@@ -35,22 +35,22 @@ export default function handler(req, res) {
         id: 'vid-07',
         caption: '💕💕💕',
         // JANGAN LUPA: Isi ID untuk video yang terakhir ini ya!
-        file_id: 'BAACAgUAAxkBAAEpC1Zp_kQ3wwEzC85NpzIV2khCmBrRKgACdhwAAo788FeCG0WLpowlhzsE' 
+        file_id: 'BAACAgUAAxkBAAEpC1Np_kHktRRWhLSDk4myyaCwIOQdsAACdRwAAo788FcPiEXe7DUvoTsE' 
       },
       {
         id: 'vid-08',
         caption: 'ada aku disini😇🤩🤩🤩',
-        file_id: 'BAACAgUAAxkBAAEpC1pp_kUExCI9990dhfwniRUnoUwrmwACdxwAAo788FcYVmB7GvjzpDsE'
+        file_id: 'BAACAgUAAxkBAAEpC1Np_kHktRRWhLSDk4myyaCwIOQdsAACdRwAAo788FcPiEXe7DUvoTsE'
       },
       {
         id: 'vid-09',
         caption: '🙂🙂🙂',
-        file_id: 'BAACAgUAAxkBAAEpC2pp_ka39s32vqeZBnnwxuxe_94IQQACeRwAAo788FeeL8OFhvv47TsE'
+        file_id: 'BAACAgUAAxkBAAEpC1Np_kHktRRWhLSDk4myyaCwIOQdsAACdRwAAo788FcPiEXe7DUvoTsE'
       },
       {
         id: 'vid-10',
         caption: 'baca yang betol😭😭😭',
-        file_id: 'BAACAgUAAxkBAAEpC15p_kXyA7TeWlIl0GkwyHQIH2Z6_wACeBwAAo788FfBt5HYAuLJaTsE'
+        file_id: 'BAACAgUAAxkBAAEpC1Np_kHktRRWhLSDk4myyaCwIOQdsAACdRwAAo788FcPiEXe7DUvoTsE'
       }
     ];
 
