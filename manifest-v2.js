559f7661ae18b0f694da6d053dbee1adb1703408
au2idsweetdanceshop-lab <au2idsweetdanceshop@gmail.com>
2026-05-11 07:39:49 +0700

    Revise manifest-v2.js with app information and icons
    Updated the manifest to include app details and new icons.

--- manifest-v2.js
+++ manifest-v2.js
@@ -1,12 +1,23 @@
-"icons": [
-  {
-    "src": "/icon-192.png",
-    "sizes": "192x192",
-    "type": "image/png"
-  },
-  {
-    "src": "/icon-512.png",
-    "sizes": "512x512",
-    "type": "image/png"
-  }
-]
+{
+  "name": "AU2Hub Community",
+  "short_name": "AU2Hub",
+  "id": "/?v=3",
+  "start_url": "/?utm_source=pwa",
+  "display": "standalone",
+  "background_color": "#0F0920",
+  "theme_color": "#0F0920",
+  "icons": [
+    {
+      "src": "/app-logo-192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any"
+    },
+    {
+      "src": "/app-logo-512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any"
+    }
+  ]
+}
